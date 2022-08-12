b8209344f2e3222988610728c8b4d91f24f655c3
jun <scorpius6361@gmail.com>
2022-08-12 14:28:47 +0900

    123

--- src/routes/Tmp.tsx
+++ src/routes/Tmp.tsx
@@ -2,37 +2,24 @@ import styles from './mydSearch/index.module.scss'
 import Button from './mydSearch/Button'
 
 export default function Tmp() {
-  const tmp1 = () => {
-    window.open('https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwjxyN24tsD5AhUJFIgKHWBECyIQPAgI')
-  }
-
-  const tmp2 = () => {
-    window.open('https://www.naver.com/')
-  }
-
-  const handlePopup = () => {
-    tmp1()
-    tmp2()
-  }
-
   const sleep = (ms: number) =>
     new Promise((resolve) => {
       setTimeout(resolve, ms)
     })
 
-  const handlePopup2 = async () => {
+  const handlePopup = async () => {
     const win = window.open() as Window
     win.location.href = 'https://www.naver.com/'
 
-    await sleep(1000).then(() => {
+    await sleep(5000).then(() => {
       win.location.href = 'https://www.daum.net/'
     })
 
-    await sleep(1000).then(() => {
+    await sleep(5000).then(() => {
       win.location.href = 'https://dribbble.com/'
     })
 
-    await sleep(1000).then(() => {
+    await sleep(5000).then(() => {
       win.close()
     })
   }
@@ -41,10 +28,6 @@ export default function Tmp() {
     <div className={styles.wrapper}>
       <Button type='button' onClick={handlePopup}>
         열기
-      </Button>
-
-      <Button type='button' onClick={handlePopup2}>
-        열기2
       </Button>
     </div>
   )
