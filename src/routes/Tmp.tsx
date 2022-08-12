2224845b14f83715bf864c31e4640485b2a333dc
jun <scorpius6361@gmail.com>
2022-08-12 13:08:38 +0900

    asdf

--- src/routes/Tmp.tsx
+++ src/routes/Tmp.tsx
@@ -7,19 +7,25 @@ export default function Tmp() {
   const ref2 = useRef<HTMLAnchorElement>(null)
 
   const handlePopup = () => {
-    // window.open('https://www.daum.net/')
+    // window.open('https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwjxyN24tsD5AhUJFIgKHWBECyIQPAgI')
     // window.open('https://www.naver.com/')
+
     ref.current?.click()
     ref2.current?.click()
   }
 
   return (
     <div className={styles.wrapper}>
-      <a href='https://myd.world/' target='_blank' rel='noreferrer' ref={ref}>
+      <a href='https://www.hyundaicard.com/index.jsp' target='_blank' rel='noreferrer' ref={ref}>
         tmp1
       </a>
 
-      <a href='https://www.daum.net/' target='_blank' rel='noreferrer' ref={ref2}>
+      <a
+        href='https://github.com/duskload/react-device-detect/blob/master/docs/selectors.md'
+        target='_blank'
+        rel='noreferrer'
+        ref={ref2}
+      >
         tmp2
       </a>
 
