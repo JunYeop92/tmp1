9650aba1ee9fb8b209bb8898abb0f65398c5a04c
jun <scorpius6361@gmail.com>
2022-08-12 10:57:27 +0900

    ewfw

--- src/routes/mydSearch/index.tsx
+++ src/routes/mydSearch/index.tsx
@@ -4,51 +4,47 @@ import Button from './Button'
 export default function MydSearch() {
   const handlePopup = () => {
     // naver
-    const win1 = window.open(
-      'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94'
-    )
+    window.open('https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94')
 
-    const win2 = window.open(
+    window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94&tqi=hXo3pdqVWdossQFZBh0ssssssFl-480932&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%95%B1%ED%85%8C%ED%81%AC'
     )
 
-    const win3 = window.open(
+    window.open(
       'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0'
     )
 
-    const win4 = window.open(
+    window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0&tqi=hYTVcwp0iIdssEHpe6VssssssMK-071731&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98'
     )
 
-    const win5 = window.open(
+    window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98&tqi=hYTVKdp0iANssEzHQ7dssssstHd-350103&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC'
     )
 
-    const win6 = window.open(
+    window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC&tqi=hYTVesp0iqdssEMuIu4ssssst%2B8-339360&query=%EB%A7%88%EC%9D%B4%EB%94%94+%ED%8F%AC%EC%9D%B8%ED%8A%B8'
     )
     // daum
-    const win7 = window.open(
-      'https://m.search.daum.net/search?q=%EB%A7%88%EC%9D%B4%EB%94%94&w=tot&nil_mtopsearch=reckwd&DA=BJE'
-    )
+    window.open('https://m.search.daum.net/search?q=%EB%A7%88%EC%9D%B4%EB%94%94&w=tot&nil_mtopsearch=reckwd&DA=BJE')
 
-    const win8 = window.open(
+    window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%95%B1%ED%85%8C%ED%81%AC'
     )
 
-    const win9 = window.open(
+    window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0'
     )
 
-    const win10 = window.open(
+    window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98'
     )
 
-    const win11 = window.open(
+    window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC'
     )
 
-    const win12 = window.open(
+    window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%ED%8F%AC%EC%9D%B8%ED%8A%B8'
     )
 
