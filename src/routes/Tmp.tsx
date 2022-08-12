c58cce7f4eb44b9b98f825a3e1360eb397034ec1
jun <scorpius6361@gmail.com>
2022-08-12 13:14:47 +0900

    wefw

--- src/routes/Tmp.tsx
+++ src/routes/Tmp.tsx
@@ -1,34 +1,16 @@
 import styles from './mydSearch/index.module.scss'
 import Button from './mydSearch/Button'
-import { useRef } from 'react'
 
 export default function Tmp() {
-  const ref = useRef<HTMLAnchorElement>(null)
-  const ref2 = useRef<HTMLAnchorElement>(null)
-
   const handlePopup = () => {
-    // window.open('https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwjxyN24tsD5AhUJFIgKHWBECyIQPAgI')
-    // window.open('https://www.naver.com/')
-
-    ref.current?.click()
-    ref2.current?.click()
+    window.open('https://www.google.com/webhp?hl=ko&sa=X&ved=0ahUKEwjxyN24tsD5AhUJFIgKHWBECyIQPAgI')
+    setTimeout(() => {
+      window.open('https://www.naver.com/')
+    }, 500)
   }
 
   return (
     <div className={styles.wrapper}>
-      <a href='https://www.hyundaicard.com/index.jsp' target='_blank' rel='noreferrer' ref={ref}>
-        tmp1
-      </a>
-
-      <a
-        href='https://github.com/duskload/react-device-detect/blob/master/docs/selectors.md'
-        target='_blank'
-        rel='noreferrer'
-        ref={ref2}
-      >
-        tmp2
-      </a>
-
       <Button type='button' onClick={handlePopup}>
         열기
       </Button>
--- src/routes/mydSearch/index.tsx
+++ src/routes/mydSearch/index.tsx
@@ -4,65 +4,68 @@ import Button from './Button'
 export default function MydSearch() {
   const handlePopup = () => {
     // naver
-    window.open('https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94')
+    const win1 = window.open(
+      'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94'
+    )
 
-    window.open(
+    const win2 = window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94&tqi=hXo3pdqVWdossQFZBh0ssssssFl-480932&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%95%B1%ED%85%8C%ED%81%AC'
     )
 
-    window.open(
+    const win3 = window.open(
       'https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0'
     )
 
-    window.open(
+    const win4 = window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0&tqi=hYTVcwp0iIdssEHpe6VssssssMK-071731&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98'
     )
 
-    window.open(
+    const win5 = window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98&tqi=hYTVKdp0iANssEzHQ7dssssstHd-350103&query=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC'
     )
 
-    window.open(
+    const win6 = window.open(
       'https://m.search.naver.com/search.naver?sm=mtb_hty.top&where=m&oquery=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC&tqi=hYTVesp0iqdssEMuIu4ssssst%2B8-339360&query=%EB%A7%88%EC%9D%B4%EB%94%94+%ED%8F%AC%EC%9D%B8%ED%8A%B8'
     )
     // daum
-    window.open('https://m.search.daum.net/search?q=%EB%A7%88%EC%9D%B4%EB%94%94&w=tot&nil_mtopsearch=reckwd&DA=BJE')
+    const win7 = window.open(
+      'https://m.search.daum.net/search?q=%EB%A7%88%EC%9D%B4%EB%94%94&w=tot&nil_mtopsearch=reckwd&DA=BJE'
+    )
 
-    window.open(
+    const win8 = window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%95%B1%ED%85%8C%ED%81%AC'
     )
 
-    window.open(
+    const win9 = window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%A7%88%EC%9D%B4%EB%8D%B0%EC%9D%B4%ED%84%B0'
     )
 
-    window.open(
+    const win10 = window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EB%8D%B0%EC%9D%B4%ED%84%B0%EA%B1%B0%EB%9E%98'
     )
 
-    window.open(
+    const win11 = window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%EC%A0%9C%ED%85%8C%ED%81%AC'
     )
 
-    window.open(
+    const win12 = window.open(
       'https://m.search.daum.net/search?nil_profile=btn&w=tot&DA=SBC&q=%EB%A7%88%EC%9D%B4%EB%94%94+%ED%8F%AC%EC%9D%B8%ED%8A%B8'
     )
 
-    // setTimeout(() => {
-    //   win1?.close()
-    //   win2?.close()
-    //   win3?.close()
-    //   win4?.close()
-    //   win5?.close()
-    //   win6?.close()
-
-    //   win7?.close()
-    //   win8?.close()
-    //   win9?.close()
-    //   win10?.close()
-    //   win11?.close()
-    //   win12?.close()
-    // }, 1000)
+    setTimeout(() => {
+      win1?.close()
+      win2?.close()
+      win3?.close()
+      win4?.close()
+      win5?.close()
+      win6?.close()
+      win7?.close()
+      win8?.close()
+      win9?.close()
+      win10?.close()
+      win11?.close()
+      win12?.close()
+    }, 1000)
   }
 
   return (
